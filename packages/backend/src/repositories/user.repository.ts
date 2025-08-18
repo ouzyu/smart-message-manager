fd0003508b2c437c0b292b8b124a2912696d250a
ouzyu <murahiro927@gmail.com>
2025-08-18 20:52:23 +0900

    仮とは言え、パスワードが露見する仕組みだったため修正

--- packages/backend/src/repositories/user.repository.ts
+++ packages/backend/src/repositories/user.repository.ts
@@ -5,14 +5,20 @@ import { prismaClient } from '@/lib/database.config';
 export const create = async (body: UserCreateRequest): Promise<UserResponse> => {
   const user = await prismaClient.user.create({
     data: body,
+    omit: {
+      password: true,
+    },
   });
 
   return user;
 };
 
-export const get = async (id: number): Promise<User | null> => {
+export const get = async (id: number): Promise<UserResponse | null> => {
   const user = await prismaClient.user.findUnique({
     where: { id },
+    omit: {
+      password: true,
+    },
   });
 
   return user;
